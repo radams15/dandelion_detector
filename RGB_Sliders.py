4a44bcf11fad79016c32da562ae4c862a460c90a
rhys <email.curly@gmail.com>
2019-05-05 16:06:16 +0100

    Initial Commit - Contains RGB slider application made by another person

---
+++ RGB_Sliders.py
@@ -0,0 +1,95 @@
+# import the necessary packages
+import numpy as np
+import cv2
+import sys
+
+image = cv2.imread(sys.argv[1])
+image = cv2.resize(image, (700, 600))
+
+def nothing(*arg):
+    pass
+
+
+def onMouse(event, x, y, flags, param):
+    # grab references to the global variables
+
+    if event == cv2.EVENT_MOUSEMOVE:
+        refPt = [(x, y)]
+        (blue, green, red) = event[refPt[0]]
+        #cv2.setTrackbarPos('mouseOver - red', 'RGB', red)
+        #cv2.setTrackbarPos('mouseOver - green', 'RGB', green)
+        #cv2.setTrackbarPos('mouseOver - blue', 'RGB', blue)
+
+
+def setBlue():
+    cv2.setTrackbarPos('lower - red', 'RGB', 86)
+    cv2.setTrackbarPos('lower - green', 'RGB', 31)
+    cv2.setTrackbarPos('lower - blue', 'RGB', 4)
+    cv2.setTrackbarPos('upper - red', 'RGB', 246)
+    cv2.setTrackbarPos('upper - green', 'RGB', 96)
+    cv2.setTrackbarPos('upper - blue', 'RGB', 57)
+
+
+cv2.namedWindow('RGB')
+#cv2.setMouseCallback('RGB', onMouse)
+cv2.createTrackbar('lower - red', 'RGB', 0, 255, nothing)
+cv2.createTrackbar('lower - green', 'RGB', 0, 255, nothing)
+cv2.createTrackbar('lower - blue', 'RGB', 0, 255, nothing)
+
+cv2.createTrackbar('upper - red', 'RGB', 1, 255, nothing)
+cv2.createTrackbar('upper - green', 'RGB', 1, 255, nothing)
+cv2.createTrackbar('upper - blue', 'RGB', 1, 255, nothing)
+
+#cv2.createTrackbar('mouseOver - red', 'RGB', 0, 255, nothing)
+#cv2.createTrackbar('mouseOver - green', 'RGB', 0, 255, nothing)
+#cv2.createTrackbar('mouseOver - blue', 'RGB', 0, 255, nothing)
+
+setBlue()
+while True:
+	# Capture frame-by-frame
+	#ret, image = cap.read()
+
+	thrs1 = cv2.getTrackbarPos('lower - red', 'RGB')
+	thrs2 = cv2.getTrackbarPos('lower - green', 'RGB')
+	thrs3 = cv2.getTrackbarPos('lower - blue', 'RGB')
+	thrs4 = cv2.getTrackbarPos('upper - red', 'RGB')
+	thrs5 = cv2.getTrackbarPos('upper - green', 'RGB')
+	thrs6 = cv2.getTrackbarPos('upper - blue', 'RGB')
+	#thrs7 = cv2.getTrackbarPos('mouseOver - red', 'RGB')
+	#thrs8 = cv2.getTrackbarPos('mouseOver - green', 'RGB')
+	#thrs9 = cv2.getTrackbarPos('mouseOver - blue', 'RGB')
+
+	if (thrs1 > thrs4):
+		cv2.setTrackbarPos('lower - red', 'RGB', thrs4 - 1)
+	if (thrs2 > thrs5):
+		cv2.setTrackbarPos('lower - green', 'RGB', thrs5 - 1)
+	if (thrs3 > thrs6):
+		cv2.setTrackbarPos('lower - blue', 'RGB', thrs6 - 1)
+
+	# define the list of boundaries
+	boundaries = [
+		([thrs3, thrs2, thrs1], [thrs6, thrs5, thrs4])
+	]
+
+	# loop over the boundaries
+	for (lower, upper) in boundaries:
+		# create NumPy arrays from the boundaries
+		lower = np.array(lower, dtype="uint8")
+		upper = np.array(upper, dtype="uint8")
+
+		# find the colors within the specified boundaries and apply
+		# the mask
+		mask = cv2.inRange(image, lower, upper)
+		output = cv2.bitwise_and(image, image, mask=mask)
+
+		imageOut = np.hstack([image, output])
+
+	# Display the resulting frame
+	cv2.imshow('RGB', imageOut)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		masking = False
+		break
+
+# When everything done, release the capture
+cv2.destroyAllWindows()
+
---
+++ main.py
@@ -0,0 +1,97 @@
+import numpy as np
+import cv2
+
+def rgb2bgr(*rgb):
+    return [rgb[2], rgb[1], rgb[0]]
+
+if __name__ == '__main__':
+    img = "image.JPG"
+
+    out_file = "out.jpg"
+    view = False
+    save = True
+
+    image = cv2.imread(img)
+
+    # more fine increase this V
+    lower, upper = [rgb2bgr(240, 0, 0), rgb2bgr(255, 255, 120)]#[rgb2bgr(233, 0, 0), rgb2bgr(255, 255, 120)]
+
+    # create NumPy arrays from the boundaries
+    lower = np.array(lower, dtype="uint8")
+    upper = np.array(upper, dtype="uint8")
+
+    """
+
+    # find the colors within the specified boundaries and apply
+    # the mask
+    mask = cv2.inRange(image, lower, upper)
+    output = cv2.bitwise_and(image, image, mask=mask)
+
+    # show the images
+    #image = cv2.resize(image, (400, 200))
+    output = cv2.resize(output, (800, 400))
+
+    #cv2.imshow("image", np.hstack([image, output]))
+
+    cv2.imshow("Filtered Image", output)
+    cv2.waitKey(0)
+    
+    """
+    height_orig, width_orig = image.shape[:2]
+
+    # output image with contours
+    image_contours = image.copy()
+
+    # DETECTING BLUE AND WHITE COLONIES
+
+    # copy of original image
+    image_to_process = image.copy()
+
+    # initializes counter
+    counter = 0
+
+    # find the colors within the specified boundaries
+    image_mask = cv2.inRange(image_to_process, lower, upper)
+    # apply the mask
+    image_res = cv2.bitwise_and(image_to_process, image_to_process, mask=image_mask)
+
+    ## load the image, convert it to grayscale, and blur it slightly
+    image_gray = cv2.cvtColor(image_res, cv2.COLOR_BGR2GRAY)
+    image_gray = cv2.GaussianBlur(image_gray, (5, 5), 0)
+
+    # perform edge detection, then perform a dilation + erosion to close gaps in between object edges
+    image_edged = cv2.Canny(image_gray, 50, 100)
+    image_edged = cv2.dilate(image_edged, None, iterations=1)
+    image_edged = cv2.erode(image_edged, None, iterations=1)
+
+    # find contours in the edge map
+    cnts = cv2.findContours(image_edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0]# if imutils.is_cv2() else cnts[1]
+
+    # loop over the contours individually
+    for c in cnts:
+        # if the contour is not sufficiently large, ignore it
+        #if cv2.contourArea(c) < 5:
+        #    continue
+
+        # compute the Convex Hull of the contour
+        hull = cv2.convexHull(c)
+
+        # prints contours in red color
+        #cv2.drawContours(image_contours, [hull], 0, (0, 0, 255), thickness=1)
+        #cv2.circle(image_contours, center=tuple(c[0][0]), color=(0,0,255), thickness=1, radius=3)
+        cv2.circle(image_contours, center=tuple(c[0][0]), color=(0,0,255), thickness=cv2.FILLED, radius=3)
+
+        counter += 1
+        # cv2.putText(image_contours, "{:.0f}".format(cv2.contourArea(c)), (int(hull[0][0][0]), int(hull[0][0][1])), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 255), 2)
+
+    # Print the number of colonies of each color
+    print(f"Counted Approximately {counter} Dandelions")
+
+    # Writes the output image
+    image_out = image_contours
+    if view:
+        cv2.imshow("Edited", image_out)
+        cv2.waitKey(0)
+    if save:
+        cv2.imwrite(out_file, image_out)
